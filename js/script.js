be9b269e5041419594193ea2dbcb0724b0acd3e6
Stas <stas3112rus@yandex.ru>
2021-09-10 19:35:46 +0300

    Adds catalog products

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
+
+
 $(document).ready(function(){
+  // Manage slider
+  // Documentation https://kenwheeler.github.io/slick/
+
     $('.slider__inner').slick({
         dots: false,
         infinite: true,
@@ -20,4 +25,33 @@ $(document).ready(function(){
           ]
         
       });
+       //End Manage slider
+
+    // Manage Tabs
+    // Documantation https://denis-creative.com/jquery-tabs/
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+      $(this)
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+     // END Manage Tabs
+    // Turn content and list
+     function ToggleSlide(nameClass){
+      $(nameClass).each(function(i){
+        $(this).on('click', function(e){
+          e.preventDefault();
+          $('.catalog__inner-content').eq(i).toggleClass('catalog__inner-content_active');
+          $('.catalog__list').eq(i).toggleClass('catalog__list_active');
+        });
+        
+      });
+     };
+     
+     ToggleSlide('.catalog__link');
+     ToggleSlide('.catalog__link-back');
+     // END Turn content and list
   });
+
+ 
+
+  
