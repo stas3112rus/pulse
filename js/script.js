904f2c3b19063d5a4cb573578b191faa3c374722
Stas <stas3112rus@yandex.ru>
2021-09-10 12:02:41 +0300

    slider for mobile

--- js/script.js
+++ js/script.js
@@ -3,12 +3,12 @@ $(document).ready(function(){
         dots: false,
         infinite: true,
         speed: 1200,
-        adaptiveHeight: true,
+        adaptiveHeight: false,
         prevArrow: '<button type="button" class="slick-prev"><img src="../img/04-slieder/chevron-left-solid.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="../img/04-slieder/chevron-right-solid.svg"></button>',
         responsive: [
             {
-              breakpoint: 768,
+              breakpoint: 900,
               settings: {
                 slidesToShow: 1,
                 slidesToScroll: 1,
